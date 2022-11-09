8e81124fd66bce123bcd436f08dab988c74e9d78
Viniciusgsc <vinycimgsc2020@gmail.com>
2022-11-08 22:27:09 -0300

    Rota tela sobre do projeto
    Rota da tela sobre do projeto

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 //IMPORT DAS PAGINAS DA APLICAÇÃO
 import Home from './pages/Home/Home'
+import Sobre from './pages/Sobre/Sobre'
 
 
 
@@ -10,7 +11,7 @@ function App() {
    //ROTAS DA APLICAÇÃO
    <Router>
       <Routes>
-    
+      <Route path="/Sobre" element={<Sobre />} />
         <Route path="/" element={<Home />} />
       </Routes>
    </Router>
